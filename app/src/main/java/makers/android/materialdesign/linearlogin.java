ca84f0664d4bbb9f3be588e8a7974a2d81c168c8
Abdul Hakim Satria Nusantara <dasisthasan@gmail.com>
2016-11-23 17:54:13 +0700

    DONE

---
+++ app/src/main/java/makers/android/materialdesign/linearlogin.java
@@ -0,0 +1,13 @@
+package makers.android.materialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class linearlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linearlogin);
+    }
+}
